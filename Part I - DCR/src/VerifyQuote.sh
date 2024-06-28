011563c901dee7440a2ff6e6909702aa870519f5
AnirudhKannan1 <akannan05@g.ucla.edu>
2024-06-28 15:09:22 -0700

    verification quote

--- Part I - DCR/src/VerifyQuote.sh
+++ Part I - DCR/src/VerifyQuote.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Empty for now...
+export SERVICE_PROVIDER_NONCE="12345678"
 
+tpm2_checkquote \
+--public rsa_ak.pub \
+--message pcr_quote.plain \
+--signature pcr_quote.signature \
+--qualification $SERVICE_PROVIDER_NONCE \
+--pcr pcr.bin
